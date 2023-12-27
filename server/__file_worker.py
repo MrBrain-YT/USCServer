b5698a084d2fcbd25020cf692ad0833d88c7e16a
MrBrain-YT <109616511+MrBrain-YT@users.noreply.github.com>
2023-12-27 20:38:44 +0300

    GitHub packages preupdate #1
    Add:
    - setup packages from GitHub

--- server/__file_worker.py
+++ server/__file_worker.py
@@ -3,9 +3,12 @@
 import configparser
 import os
 import tarfile
+import re
 import shutil
-import time
+import stat
+from subprocess import call
 
+import git
 from flask import request, send_file, jsonify, render_template
 
 def file_worker(app):
@@ -123,3 +126,82 @@ def file_worker(app):
         with open(config_file, 'w') as config_file:
             packages_config.write(config_file)
         return jsonify({'message': f'Deleted item: {package_name}'}), 200
+    
+    
+    
+    def on_rm_error(func, path, exc_info):
+        #from: https://stackoverflow.com/questions/4829043/how-to-remove-read-only-attrib-directory-with-python-in-windows
+        os.chmod(path, stat.S_IWRITE)
+        os.unlink(path)
+
+
+    @app.route('/from_repo', methods=['POST'])
+    def from_repo():
+        # set global variables
+        error = False
+        package_name = ""
+        
+        # get url
+        data = request.get_json()
+        url = data.get('url')
+        dir = f'{os.path.dirname(os.path.abspath(__file__))}/temp/git'
+        
+
+        # Проверяем, что URL является ссылкой на GitHub
+        github_url_pattern = r'^https?://github\.com/.+/.+\.git$'
+        if not re.match(github_url_pattern, url):
+            return jsonify({'error': 'Invalid GitHub repository URL'}), 400
+
+
+        # Клонируем репозиторий по указанному URL
+        git.Repo.clone_from(url, f'{os.path.dirname(os.path.abspath(__file__))}/temp/git')
+        
+        # Check package
+        dir_path = f"{os.path.dirname(os.path.abspath(__file__))}/temp/git"
+        package_dir_path = f"{dir_path}/{[folder for folder in os.listdir(dir_path) if os.path.isdir(f"{dir_path}/{folder}")][1]}"
+        if os.path.exists(f"{package_dir_path}/package.ini"):
+            
+            config = configparser.ConfigParser()
+            config.read(f"{package_dir_path}/package.ini")
+            package_name = config["INFO"].get("name")
+            package_version = config["INFO"].get("version")
+            if package_name and package_version != None:
+                print("2_yes")
+                # add package to list
+                config_file = f"{os.path.dirname(os.path.abspath(__file__))}/packages/packages.ini"
+                packages_config = configparser.ConfigParser()
+                packages_config.read(config_file)
+                packages_config[package_name] = {
+                    "name" : package_name,
+                    "version" : package_version
+                }
+                with open(config_file, 'w') as configfile:
+                    packages_config.write(configfile)
+                
+                # move dir to packages
+                shutil.move(package_dir_path, f"{os.path.dirname(os.path.abspath(__file__))}/packages")
+                
+            else:
+                error = True
+        else:
+            error = True
+
+        
+        
+        # delete .git folder
+        for i in os.listdir(dir):
+            if i.endswith('git'):
+                tmp = os.path.join(dir, i)
+                # We want to unhide the .git folder before unlinking it.
+                while True:
+                    call(['attrib', '-H', tmp])
+                    break
+                shutil.rmtree(tmp, onerror=on_rm_error)
+        # delete git folder
+        shutil.rmtree(dir)
+        if not error:
+            return jsonify({'message': f'Added item: {package_name}'}), 200
+        else:
+            return jsonify({'message': "error"}), 500
+            
+        
